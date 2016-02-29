2f25f899b109f8645b776299b7c0fca299d94cfc
jfm Meyers <peterpan0413@live.com>
2016-02-29 13:08:10 -0600

    Initial commit

---
+++ flashasm.g4
@@ -0,0 +1,45 @@
+grammar flashasm;
+fragment INTEGER: [0-9]+;
+fragment SPACES: ' '*;
+fragment ANYTHING: (.*?)'\n';
+PROGRAM: 'program';
+BODY: 'body';
+CODE: 'code';
+TRY: 'try';
+SCRIPT: 'script';
+MAXSTACK: 'maxstack ' INTEGER;
+REFID: 'refid \"'.*?'\"';
+RETURNVIOD: 'returnvoid';
+LOCALCOUNT: 'localcount ' INTEGER;
+INITSCOPEDEPTH: 'initscopedepth ' INTEGER;
+MAXSCOPEDEPTH: 'maxscopedepth ' INTEGER;
+GETLOCAL0: 'getlocal0';
+PUSHSCOPE: 'pushscope';
+POPSCOPE: 'popscope';
+END : 'end';
+MINORVERSION: 'minorversion ' INTEGER;
+MAJORVERSION: 'majorversion ' INTEGER;
+SINIT: 'sinit';
+GETSCOPEOBJECT: 'getscopeobject' SPACES INTEGER;
+GETLEX: 'getlex' ANYTHING;
+NEWCLASS: 'newclass' SPACES '\"'(.*?)'\"';
+INITPROPERTY:'initproperty' SPACES ANYTHING;
+TRAIT:'trait' ANYTHING;
+INCLUDE: '#include ''\"'(.*?)'\"';
+
+flashasm: program EOF;
+program: PROGRAM MINORVERSION MAJORVERSION (script)* (trait)* END;
+script: SCRIPT SINIT REFID (body)* END;
+body: BODY MAXSTACK LOCALCOUNT INITSCOPEDEPTH MAXSCOPEDEPTH (code)* END ;
+code: CODE internalcode END;
+internalcode: (GETLOCAL0 |POPSCOPE |PUSHSCOPE |GETSCOPEOBJECT |GETLEX |NEWCLASS|INITPROPERTY |RETURNVIOD |TRAIT)+;
+trait: TRAIT INCLUDE END;
+
+//WS : (.+) -> skip;
+
+WS
+   : (' ' | '\t' | '\n' | '\r') -> skip
+   ;
+LINE_COMMENT
+   : ';' ~ ('\n' | '\r')* '\r'? '\n' -> skip
+   ;
---
+++ gen/flashasmBaseListener.java
@@ -0,0 +1,122 @@
+// Generated from C:/Users/peter/Downloads/flashasm parser\flashasm.g4 by ANTLR 4.5.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link flashasmListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class flashasmBaseListener implements flashasmListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFlashasm(flashasmParser.FlashasmContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFlashasm(flashasmParser.FlashasmContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(flashasmParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(flashasmParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterScript(flashasmParser.ScriptContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitScript(flashasmParser.ScriptContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBody(flashasmParser.BodyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBody(flashasmParser.BodyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCode(flashasmParser.CodeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCode(flashasmParser.CodeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInternalcode(flashasmParser.InternalcodeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInternalcode(flashasmParser.InternalcodeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTrait(flashasmParser.TraitContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTrait(flashasmParser.TraitContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
